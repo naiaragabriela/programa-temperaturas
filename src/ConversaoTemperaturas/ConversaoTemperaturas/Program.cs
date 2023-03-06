5eec89bf756719c67060b0a0fa002ce7d0f43cf5
naiaragabriela <naigabi31@gmail.com>
2023-03-05 21:51:25 -0300

    conversao de temperatura usando funções

---
+++ src/ConversaoTemperaturas/ConversaoTemperaturas/Program.cs
@@ -0,0 +1,16 @@
+﻿double celsius, fahrenheit;
+
+double TemperaturaDigitada()
+{
+    Console.Write("Digite um valor de temperatura em Graus Celcius: ");
+    return double.Parse(Console.ReadLine());
+}
+
+celsius = TemperaturaDigitada();
+ 
+void TemperaturaConvertida()
+{
+    fahrenheit = (((celsius * 9) / 5) + 32);
+    Console.WriteLine($"A temperatura {celsius} ºC é convertida para {fahrenheit} ºF");
+}
+TemperaturaConvertida();
